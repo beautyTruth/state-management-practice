b5ad4078a71c8c99ffd56a4ee1eb5b98a2ad0504
Phil <happy.melodist@gmail.com>
2022-04-24 22:12:14 -0500

    complete canvas set up in script.js

---
+++ state-management-games/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+  const loading = document.querySelector("#loading");
+  loading.style.display = "none";
+  const canvas = document.querySelector("#bathtubFarts");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+// i should have been a pair of ragged claws scuttling along the floors of silent seas
